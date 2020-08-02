649187992198e22ab8f6a0b995c5e0c17a5d145c
Robson <robson@masterhub.com.br>
2020-08-02 11:41:56 -0300

    ..

--- www/js/config.js
+++ www/js/config.js
@@ -3,7 +3,7 @@
 var krms_driver_config ={		
 	'ApiUrl':"https://osasfood.com.br/driver/api",	
 	'DialogDefaultTitle':"Osasfood Delivery",
-	'mapboxToken' : 'pk.eyJ1Ijoib3Nhc2Zvb2QiLCJhIjoiY2tkMDM1c3loMDlnYTJ6cGN5bmQ0ZDJ6bSJ9.yc-cHBkaTtzRc4V6f5JdIQ',
+	'mapboxToken' : 'pk.eyJ1Ijoib3Nhc2Zvb2QiLCJhIjoiY2tkZDZ3ZWZkMHNneDJycXRxdGF3Z3k0OSJ9.nbgxTXaRzSxe8BJrvacU6w',
 	'APIHasKey':"vvfwaszXcCJb1WO9Cd831nsbEHs=",
-	'debug': true 
+	'debug': false 
 };
